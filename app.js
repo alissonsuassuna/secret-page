873436246e83828ce7352809da0763d417e9f637
alissonsuassuna <alissonsuassuna@gmail.com>
2024-02-26 15:36:07 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const ejs = require('ejs')
+const mongoose = require('mongoose')
+const encrypt = require('mongoose-encryption')
+
+const app = express()
+
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/userDB')
+
+const userSchema = new mongoose.Schema( {
+    email: String,
+    password: String
+})
+
+const secret = 'Tpoihgfkjhgvcrbvcrjhgfduygf'
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password']});
+
+const User = new mongoose.model('User', userSchema)
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+
+app.post('/register', (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    })
+
+    newUser.save()
+    res.render('secrets')
+})
+
+app.post('/login', async (req, res) => {
+    const userName = req.body.username
+    const password = req.body.password
+
+    try {
+
+        const resultUser = await User.findOne({email: userName}).exec()
+        if(resultUser) {
+            res.render('secrets')
+        } else {
+            res.send('<h2>Senha ou e-mail incorretos</h2>')
+        }
+    } catch (error) {
+        console.log('Erro no sistema', error)
+    }
+})
+
+
+app.listen(4000, () => {
+    console.log('Servidor Rodando na porta 4000')
+})
